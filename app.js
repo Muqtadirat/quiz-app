a9bac74a822d66be7b35d3548bb502262899292d
Muqtadirat <muqtadir.my@gmail.com>
2023-12-28 10:45:27 +0100

    feat: multiple questions &  score display

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const apiUrl = `https://quizapi.io/api/v1/questions?apiKey=${apiKey}&category=co
 const container = document.querySelector(".container");
 const modal = document.querySelector(".modal");
 const feedback = document.querySelector(".modal-content");
+const timer = document.querySelector(".timer");
 
 function createQuiz() {
   const quizBody = document.createElement("section");
@@ -37,64 +38,103 @@ function createQuiz() {
 
 createQuiz();
 
-const question = document.querySelector(".question");
+const questionText = document.querySelector(".question");
 const optionA = document.querySelector(".option-a button");
 const optionB = document.querySelector(".option-b button");
 const optionC = document.querySelector(".option-c button");
 const optionD = document.querySelector(".option-d button");
+const scoreBody = document.querySelector(".score");
+
 let correctAnswer = "";
+let score = 0;
 
-const setQuiz = (response) => {
-  for (const key in response.data[0].correct_answers) {
-    if (response.data[0].correct_answers[key] === "true") {
-      correctAnswer = key;
-      break;
+//Display question
+function handleQuiz(response) {
+  let currentQuestionIndex = 0;
+  const questions = response.data;
+
+  const setQuiz = (index) => {
+    const currentQuestion = questions[index];
+
+    questionText.textContent = `${currentQuestion.question}`;
+    optionA.textContent = `A) ${currentQuestion.answers.answer_a}`;
+    optionB.textContent = `B) ${currentQuestion.answers.answer_b}`;
+    optionC.textContent = `C) ${currentQuestion.answers.answer_c}`;
+    optionD.textContent = `D) ${currentQuestion.answers.answer_d}`;
+
+    for (const key in currentQuestion.correct_answers) {
+      if (currentQuestion.correct_answers[key] === "true") {
+        correctAnswer = key;
+        break;
+      }
     }
+  };
+
+  setQuiz(currentQuestionIndex);
+
+  optionA.addEventListener("click", () => {
+    checkAnswer("answer_a_correct");
+  });
+  optionB.addEventListener("click", () => {
+    checkAnswer("answer_b_correct");
+  });
+  optionC.addEventListener("click", () => {
+    checkAnswer("answer_c_correct");
+  });
+  optionD.addEventListener("click", () => {
+    checkAnswer("answer_d_correct");
+  });
+
+  //user feedback for answers
+  const checkAnswer = (selectedAnswer) => {
+    if (selectedAnswer === correctAnswer) {
+      modal.style.display = "flex";
+      feedback.textContent = "Yayy, correct!";
+      feedback.style.backgroundColor = "#00FF00";
+      score++
+      scoreBody.innerHTML = score;
+    } else {
+      modal.style.display = "flex";
+      feedback.textContent = "Oof, wrong!";
+      feedback.style.backgroundColor = "#FF0000";
+    }
+
+    console.log(score);
+
+    setTimeout(() => {
+      modal.style.display = "none";
+
+      // Move to next question after a delay
+      setTimeout(() => {
+        currentQuestionIndex++;
+
+        if (currentQuestionIndex < questions.length) {
+          setQuiz(currentQuestionIndex);
+        } else {
+          alert("Quiz completed!");
+          // You might want to handle completion logic here
+        }
+      }, 500); // Adjust the delay as needed
+    }, 2000);
+  };
+}
+
+let seconds = 60;
+const handleTimer = setInterval(() => {
+  seconds--;
+  timer.textContent = seconds;
+  if (seconds === 0) {
+    seconds = 60;
+  } else if (seconds <= 15) {
+    timer.style.color = "red";
   }
-
-  //   correctAnswer = response.data[0].correct_answer;
-  console.log(correctAnswer);
-  question.textContent = `${response.data[0].question}`;
-
-  optionA.textContent = `A) ${response.data[0].answers.answer_a}`;
-  optionB.textContent = `B) ${response.data[0].answers.answer_b}`;
-  optionC.textContent = `C) ${response.data[0].answers.answer_c}`;
-  optionD.textContent = `D) ${response.data[0].answers.answer_d}`;
-};
-
-optionA.addEventListener("click", () => {
-  checkAnswer("answer_a_correct");
-});
-optionB.addEventListener("click", () => {
-  checkAnswer("answer_b_correct");
-});
-optionC.addEventListener("click", () => {
-  checkAnswer("answer_c_correct");
-});
-optionD.addEventListener("click", () => {
-  checkAnswer("answer_d_correct");
-});
-
-//user feedback for answers
-const checkAnswer = (selectedAnswer) => {
-  if (selectedAnswer === correctAnswer) {
-    modal.style.display = "flex";
-    feedback.textContent = "Yayy, correct!";
-    feedback.style.backgroundColor = "#00FF00";
-    alert("Correct");
-  } else {
-    modal.style.display = "flex";
-    feedback.textContent = "Oof, wrong!";
-    feedback.style.backgroundColor = "#FF0000";
-  }
-
-  console.log(selectedAnswer);
-};
+}, 1000);
 
 axios
   .get(apiUrl)
   .then((response) => {
-    setQuiz(response);
+    // setQuiz(response);
+    handleQuiz(response);
   })
   .catch((error) => {
     console.error("Error fetching data:", error);
